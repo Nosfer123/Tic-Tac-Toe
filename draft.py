3e52bd3fe56d0e202a6c53b1ae2373f6f4dcb9b4
Dmytro <dmytro.chebotarev@gmail.com>
2017-05-15 12:28:12 +0300

    add comments (game.py)

--- draft.py
+++ draft.py
@@ -1,8 +1,2 @@
-restart_game = input('Do you want to restart game? (yes/no):\n')
-print(restart_game)
-while restart_game != 'yes' or 'no':
-    restart_game = input()
-if restart_game == 'yes':
-    print('yes')
-else:
-    print('no')
+board = {a: a + 1 for a in range(9)}
+print(board)
--- game.py
+++ game.py
@@ -3,9 +3,10 @@
 # Player can go again to the occupied cell
 
 
-board={'1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9'} #Starting conditions
+board = {'1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9'} #Starting conditions
 turn = 'player1' #Starting conditions
 letter = 'X' #Starting conditions
+
 
 def drawboard():
     print('   |   |')
@@ -20,6 +21,7 @@ def drawboard():
     print(' ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'])
     print('   |   |\n')
 
+
 def restart(): #not finished
     restart_game = input('Do you want to restart game? (yes/no):\n').lower()
 #    while restart_game != 'yes' or restart_game != 'no': #why this cycle never ends?
@@ -33,8 +35,7 @@ def restart(): #not finished
         exit()
 
 
-
-def Win():
+def is_win():
     global letter # is it ok to use global in this situation, or better to have local argument?
     if (board['1'] == letter and board['2'] == letter and board ['3'] == letter or # top line
     board['4'] == letter and board['5'] == letter and board['6'] == letter or      # second line
@@ -51,16 +52,19 @@ def Win():
         return restart()
 
 
-
 while True:
     drawboard()
-    Win()
+    is_win()
     if turn == 'player1':
         letter = 'X'
         move = input('Player 1 turn (Select a cell: 1-9): ')
+#        while board[move] == 'X' or '0': #why this cycle never ends?
+#            move = input('Player 1 turn (Select a cell: 1-9): ')
         turn = 'player2'
     else:
         letter = '0'
         move = input('Player 2 turn (Select a cell: 1-9): ')
+#        while board[move] == 'X' or '0': #why this cycle never ends?
+#            move = input('Player 1 turn (Select a cell: 1-9): ')
         turn = 'player1'
     board[move] = letter
