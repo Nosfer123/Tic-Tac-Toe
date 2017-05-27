eee898b44910f9fd5ba7e13cea8654c721acb286
Dmytro <dmytro.chebotarev@gmail.com>
2017-05-27 13:48:27 +0300

    - changed dict to list (in all code) - changed board drawing (using for) - changed is_win() - add test of possible movement

--- game.py
+++ game.py
@@ -1,23 +1,25 @@
 # Now game is playable for two players.
 
 # Starting conditions
-board = {'1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9'}
-turn = 'player1'
+board = [[str(a+1+(b*3)) for a in range(3)] for b in range(3)]
+turn = 'Player 1'
 letter = 'X'
+move_counter = 0
 
 
 def draw_board():
-    print('   |   |')
-    print(' ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board['4'] + ' | ' + board['5'] + ' | ' + board['6'])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'])
-    print('   |   |\n')
+    num_of_lst = 0
+    end = '-----------'
+    is_end_board = 0
+    for dummy_n in range(3):
+        print('   |   |')
+        print(' ' + board[num_of_lst][0] + ' | ' + board[num_of_lst][1] + ' | ' + board[num_of_lst][2])
+        print('   |   |')
+        print(end)
+        num_of_lst += 1
+        is_end_board += 1
+        if is_end_board == 2:
+            end = '\n'
 
 
 def restart():
@@ -28,12 +30,13 @@ def restart():
         else:
             break
     if restart_game == 'yes':
-        global board, turn, letter
+        global board, turn, letter, move_counter
         cls()  # bad solution, but "import os      os.system('cls' if os.name == 'nt' else 'clear')" don't work
-        board = {'1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9'}
+        board = [[str(a + 1 + (b * 3)) for a in range(3)] for b in range(3)]
         draw_board()
-        turn = 'player1'
+        turn = 'Player 1'
         letter = 'X'
+        move_counter = 0
     if restart_game == 'no':
         exit()
 
@@ -45,47 +48,61 @@ def cls():  # need to clean console after restart
 def is_win():
     global letter  # is it ok to use global in this situation, or better to have local argument?
 
-    if (board['1'] == letter and board['2'] == letter and board['3'] == letter or     # top line
-        board['4'] == letter and board['5'] == letter and board['6'] == letter or      # second line
-        board['7'] == letter and board['8'] == letter and board['9'] == letter or      # last line
-        board['1'] == letter and board['4'] == letter and board['7'] == letter or      # first column
-        board['2'] == letter and board['5'] == letter and board['8'] == letter or      # second column
-        board['3'] == letter and board['6'] == letter and board['9'] == letter or      # third column
-        board['1'] == letter and board['5'] == letter and board['9'] == letter or      # diagonal
-            board['3'] == letter and board['5'] == letter and board['7'] == letter):   # diagonal
+    zipped = list(zip(board[0], board[1], board[2]))
+    zipped_list = []
+    for a in range(3):  # from tuple to list
+        zipped_list.append(list(zipped[a]))
+    diag_lst = [[board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]]]
+    win_list = board + zipped_list + diag_lst
 
-        if letter == 'X':
-            print('Player 1 win')
-        else:
-            print('Player 2 win')
-        return restart()
+    for lst in range(8):
+        if win_list[lst].count(letter) == 3:
+            if letter == 'X':
+                print('Player 1 win')
+            else:
+                print('Player 2 win')
+            return restart()
+
+
+def checking_board():  # check if this part of the board is exist
+    is_letter_in_lst = False
+    global move
+    while is_letter_in_lst is False:
+        for n in range(3):
+            if move in board[n]:
+                a = board[n].index(move)
+                board[n][a] = letter
+                is_letter_in_lst = True
+                break
+        if is_letter_in_lst is False:
+            move = str(input(turn + ' turn (Select a cell: 1-9): '))
+
+
+def is_empty_cell(move_counter):
+    if move_counter == 9:
+        print('There is no winner in this game')
+        restart()
 
 
 while True:
     draw_board()
     is_win()
-    if turn == 'player1':
+    is_empty_cell(move_counter)
+    if turn == 'Player 1':
         letter = 'X'
-        while True:
-            try:
-                move = input('Player 1 turn (Select a cell: 1-9): ')
-                while board[move] == 'X' or board[move] == '0':
-                    move = input('This cell is occupied. Player 1 turn (Select a cell: 1-9): ')
-            except KeyError:
-                print("It's possible to choose only cells 1-9 except occupied")
-            else:
-                break
-        turn = 'player2'
+        move = str(input('Player 1 turn (Select a cell: 1-9): '))
+        while move == 'X' or move == 'Y':
+            move = str(input('Player 1 turn (Select a cell: 1-9): '))
+        checking_board()
+        turn = 'Player 2'
+        move_counter += 1
+
     else:
         letter = '0'
-        while True:
-            try:
-                move = input('Player 2 turn (Select a cell: 1-9): ')
-                while board[move] == 'X' or board[move] == '0':
-                    move = input('This cell is occupied. Player 2 turn (Select a cell: 1-9): ')
-            except KeyError:
-                print("It's possible to choose only cells 1-9 except occupied")
-            else:
-                break
-        turn = 'player1'
-    board[move] = letter
+        move = str(input('Player 2 turn (Select a cell: 1-9): '))
+        while move == 'X' or move == 'Y':
+            move = str(input('Player 2 turn (Select a cell: 1-9): '))
+        checking_board()
+        turn = 'Player 1'
+        move_counter += 1
+    print(move_counter)
